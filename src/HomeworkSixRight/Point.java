072ab57533fe9d80d84dda85439ad3bc9f889c60
borzdykooa <borzdykooa@mail.ru>
2018-02-01 11:40:49 +0300

    HomeworkSixRight

---
+++ src/HomeworkSixRight/Point.java
@@ -0,0 +1,34 @@
+package HomeworkSixRight;
+
+public class Point {
+    
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt((point.x - this.x) * (point.x - this.x) + (point.y - this.y) *
+                (point.y - this.y));
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+
---
+++ src/HomeworkSixRight/RectangleDemo.java
@@ -0,0 +1,10 @@
+package HomeworkSixRight;
+
+public class RectangleDemo {
+
+    public static void main(String[] args) {
+        Rectangle firstRectangle = new Rectangle(new Point(-3, 5), new Point(6, -1));
+        System.out.println("Диагональ прямоугольника составляет " + firstRectangle.diagonal());
+        System.out.println("Площадь прямоугольника составляет " + firstRectangle.area());
+    }
+}
